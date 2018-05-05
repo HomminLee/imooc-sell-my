50ccdfacfed4a17a6070950f2cc45c1628f00e51
hommin <hommin84255@163.com>
2018-05-05 16:33:33 +0800

    卖家端页面

--- src/main/java/com/hommin/study/imoocsell/controller/SellerOrderController.java
+++ src/main/java/com/hommin/study/imoocsell/controller/SellerOrderController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +18,6 @@ import java.util.Map;
 
 /**
  * @author Hommin
- * @ClassName: SellerOrderController
- * @Description:
- * @data 2018年04月19日 下午9:44
  */
 @Controller
 @RequestMapping("/seller/order")
